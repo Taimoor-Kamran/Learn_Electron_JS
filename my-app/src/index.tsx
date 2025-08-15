9ffe921bc345e554ca783429ac595f1b146c7ff1
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-08-15 17:46:33 +0500

    added 13 commit

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter,  Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <BrowserRouter>
-    <Routes></Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
 
